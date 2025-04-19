a301db32be5bbe9a31d376398fd9cba702066d5e
Jok <alexkofford@gmail.com>
2025-04-19 01:12:25 -0600

    Init for modular tools

---
+++ ModularTools/ModEntry.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Buildings;
+
+namespace ModularTools
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        public static IModHelper Helper;
+        
+        public override void Entry(IModHelper helper)
+        {
+            Helper = helper;
+            //Helper.Events.Input.ButtonPressed += OnButtonPressed;
+            HarmonyPatches();
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (e.Button == SButton.C)
+            {
+                FishPond pond = new FishPond();
+            }
+        }
+
+        private void HarmonyPatches()
+        {
+            var harmony = new Harmony(ModManifest.UniqueID);
+        }
+        
+    }
+}
